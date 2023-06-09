d32e0519ad3a520a04dfce07f9a8adefcd2b337f
zhaowanyi <yugemio0721@outlook.com>
2023-06-09 20:49:05 +0800

    完成了登录和注册功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,7 +1,10 @@
 // 注意：每次调用 $.get() 或 $.post() 或 $.ajax() 的时候，
 // 会先调用 ajaxPrefilter 这个函数
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
-$.ajaxPrefilter(function(options) {
+$.ajaxPrefilter(function (options) {
   // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
-  options.url = 'http://ajax.frontend.itheima.net' + options.url
+  // options.url = 'http://ajax.frontend.itheima.net' + options.url
+  options.url = 'http://api-breakingnews-web.itheima.net' + options.url
+  // options.url = 'http://www.liulongbin.top:3007' + options.url
+  // options.url = 'http://www.liulongbin.top:3007' + options.url
 })
